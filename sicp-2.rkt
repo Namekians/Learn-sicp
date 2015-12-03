49b1c73d1ada44c54109e3e214430acb711d8c50
Namekians <yinchenhao0@gmail.com>
2015-12-03 01:04:36 -0500

    begin 2nd chapter

--- sicp-2.rkt
+++ sicp-2.rkt
@@ -5,7 +5,10 @@
   (display (numer x))
   (display "/")
   (display (denom x)))
-(define (make-rat a b)(cons a b))
+(define (make-rat a b)
+  (let ((g ((if (< b 0)  - +)(abs (gcd a b)))))(cons (/ a g)(/ b g)))
+)
+  
 (define (numer x)(car x))
 (define (denom x)(cdr x))
 
@@ -13,3 +16,10 @@
 (if (= b 0)
 a
 (gcd b (remainder a b))))
+
+(define (abs x)(cond ((> x 0) x)
+                     ((< x 0)(- x))
+                     (else 0)
+                     ))
+
+
